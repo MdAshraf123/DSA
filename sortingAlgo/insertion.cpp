b4699c0d6790003f221c1a2bc6b96319fbb6fc99
Md Ashraf <slopdgh1234567@gmail.com>
2024-09-14 13:24:17 +0530

    adding binary tree

--- sortingAlgo/insertion.cpp
+++ sortingAlgo/insertion.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 using namespace std;
 void insertionSort(int arr[],int size){
-    int i=1,j=0,item;
-    for(i=1;i<size;i++){
-         item=arr[i];
-        for(j=i-1;j>=0;j--){
-            if(arr[j]>item){
+    int i=1,j=i-1,key;
+    for(;i<size;i++){
+         key=arr[i];
+        for(;j>=0;j--){
+            if(arr[j]>key){
                 arr[j+1]=arr[j];
             }else{
                 break;
             }
         }
-        arr[j+1]=item;
+        arr[j+1]=key;
     }
     for(int p=0;p<size;p++){
         cout<<arr[p]<<' ';
@@ -20,13 +20,13 @@ void insertionSort(int arr[],int size){
 void insertionSort1(int a[],int size){
     int i,j;
     for(i=1;i<size;i++){
-        int item=a[i];
+        int key=a[i];
         j=i-1;
-        while(j>=0 and a[j]>item){
+        while(j>=0 and a[j]>key){
                 a[j+1]=a[j];
             j--;
         }
-        a[j+1]=item;
+        a[j+1]=key;
     }
     for(int p=0;p<size;p++){
         cout<<a[p]<<' ';
@@ -34,6 +34,6 @@ void insertionSort1(int a[],int size){
 }
 int main(){
     int a[5]={5,80,2,10,7};
-    insertionSort1(a,5);
+    insertionSort(a,5);
     return 0;
 }
---
+++ tree.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+using namespace std;
+ 
+struct Node{
+    int data;
+    Node * left;
+    Node * right;
+    Node(int a){
+        data=a;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+Node * createTree(Node * root){
+    int x=0;
+    cout<<"Enter data- ";
+    cin>>x;
+    if(x==-1){
+        return NULL;
+    }
+    root=new Node(x);
+    cout<<"Enter data in left of "<<x<<endl;
+    root->left=createTree(root->left);
+    cout<<"Enter data in right of "<<x<<endl;
+    root->right=createTree(root->right);
+    return root;
+}
+
+Node* insertNode(Node * root,int data){
+    if(root==NULL){
+        root=new Node(data);
+        return root;
+    }
+    if(data < root->data){
+        root->left=insertNode(root->left,data);
+    }else{
+        root->right=insertNode(root->right,data);
+    }
+    return root;
+}
+
+void inOrderTraversal(Node * root){
+    if(root==NULL){
+        return ;
+    }
+    inOrderTraversal(root->left);
+    cout<<root->data<<" ";
+    inOrderTraversal(root->right);
+}
+void preOrderTraversal(Node * root){
+    if(root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+void postOrderTraversal(Node * root){
+    if(root==NULL){
+        return ;
+    }
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main(){
+Node * root=NULL;
+root= createTree(root);
+inOrderTraversal(root);
+    return 0;
+}
